913375d45010547b8f4aef9ca02a7f298f0924d9
Claude Heiland-Allen <claudiusmaximus@goto10.org>
2012-02-08 11:32:27 +0000

    fix branch name

--- src/start.sh
+++ src/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git checkout -b "session-$(date --iso=s)"
+git checkout -b "session-$(date -u +%F-%H%M%S)"
 make
 xterm -geometry 80x5+0+0    -T live -e bash -c 'while true ; do ./live | ts ; mplayer -quiet -really-quiet -ao jack $(ls -1 ../doh/*.wav | sort -R | head -n 1) >/dev/null 2>&1 ; done' &
 xterm -geometry 80x25+0+102 -T make -e bash -c 'while true ; do make -q || ( git add go.c ; git commit -m "go $(date --iso=s)" ; make --quiet ) ; sleep 1 ; done' &
