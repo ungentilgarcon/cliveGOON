6e56e104b42a3b6ec6c521b0a6354ead1727fb50
Claude Heiland-Allen <claudiusmaximus@goto10.org>
2012-02-10 14:54:32 +0000

    no ts; window pos

--- src/start.sh
+++ src/start.sh
@@ -4,8 +4,8 @@ git checkout -b "${SESSION}"
 make
 ecasound -G:jack,record -f:f32,1,48000 -i:jack -o "${SESSION}.wav" &
 sleep 5
-xterm -geometry 80x5+0+0    -T live -e bash -c 'while true ; do ./live 2>&1 | ts ; mplayer -quiet -really-quiet -ao jack $(ls -1 ../doh/*.wav | sort -R | head -n 1) >/dev/null 2>&1 ; done' &
-xterm -geometry 80x25+0+102 -T make -e bash -c 'while true ; do make -q || ( git --no-pager diff --color ; git add go.c ; git commit -m "go $(date --iso=s)" ; make --quiet ) ; sleep 1 ; done' &
+xterm -geometry 80x5+0+0    -T live -e bash -c 'while true ; do ./live ; mplayer -quiet -really-quiet -ao jack $(ls -1 ../doh/*.wav | sort -R | head -n 1) >/dev/null 2>&1 ; done' &
+xterm -geometry 80x25+0+87  -T make -e bash -c 'while true ; do make -q || ( git --no-pager diff --color ; git add go.c ; git commit -m "go $(date --iso=s)" ; make --quiet ) ; sleep 1 ; done' &
 xterm -geometry 80x25+0+449 -T htop -e bash -c 'htop' &
 xterm -geometry 80x56+493+0 -T edit -e bash -c 'while true ; do nano ./go.c ; done' &
 wait
