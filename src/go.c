5cbbec82b44e273999b8c99af08077a7c3157c2d
ungentilgarçon <gregory.bahde@laposte.net>
2018-10-31 00:43:50 +0100

    cliveGOON is alive for halOw33N*

--- src/go.c
+++ src/go.c
@@ -1,16 +1,14 @@
 #define SR 48000
 #include "dsp.h"
-
 typedef struct {
-  int reloaded;
+int reloaded;
+PHASOR clock, osc;
 } S;
-
 int go(S *s, int channels, const float *in, float *out) {
-  if (s->reloaded) {
-    s->reloaded = 0;
-  }
-  for (int c = 0; c < channels; ++c) {
-    out[c] = 0;
-  }
-  return 0;
+if (s->reloaded) { s->reloaded = 0; }
+double env = phasor(&s->clock, 125/60.0) < 0.25;
+double osc = sin(twopi * phasor(&s->osc, 440));
+double o = env * osc;
+for (int c = 0; c < channels; ++c) { out[c] = o; }
+return 0;
 }
